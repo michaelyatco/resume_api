cdb2ba308b6b8a7a839c9979c81f978b8f2f7940
Michael Yatco <mjyatco@gmail.com>
2017-03-12 09:43:47 -0400

    added users,sessions controllers, routes, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#users routes
+  get "/signup", to: "users#new"
+  post "/users", to: "users#create"
+
+#sessions routes
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
 end
