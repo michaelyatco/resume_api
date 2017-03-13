dac3cf555493292322bd96204ae7a3ec57f59ead
James Chou <jameschou1993@gmail.com>
2017-03-13 18:32:45 -0400

    put students.create at the beginning of seed file and compelted destroy and update methods

--- app/controllers/api/v1/resume_controller.rb
+++ app/controllers/api/v1/resume_controller.rb
@@ -9,13 +9,12 @@ class Api::V1::ResumeController < ApplicationController
 
   def update
     student =  Student.find_by(id: params[:id])
+    student.assign_attribute(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number], short_bio: params[:short_bio], linkedin_url: params[:linkedin_url], twitter_handle: params[:twitter_handle], personal_website_url: params[:personal_website_url], online_resume_url: params[:online_resume_url], github_url: params[:github_url], photo: params[:photo])
 
     params[:experiences].each do |experience_hash|
       experience = Experience.find_by(id: experience_hash[:id])
       experience.update(start_date: experience_hash[:start_date])
     end
-
-    student.update(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number], short_bio: params[:short_bio], linkedin_url: params[:linkedin_url], twitter_handle: params[:twitter_handle], personal_website_url: params[:personal_website_url], online_resume_url: params[:online_resume_url], github_url: params[:github_url], photo: params[:photo])
 
     params[:skills].each do |skills_hash|
       skill = Skill.find_by(id: skills_hash[:id])
@@ -23,11 +22,21 @@ class Api::V1::ResumeController < ApplicationController
     end
 
     params[:educations].each do |educations_hash|
-
+      educations = Education.find_by(id: educations_hash[:id])
+      educations.update(start_date: params[:start_date], end_date: params[:end_date], degree: params[:degree], university: params[:university], details: params[:details])
+    end
+    params[:capstones].each do |capstones_hash|
+      capstones = Capstone.find_by(id: capstones_hash[:id])
+      capstones.update(name: params[:name], description: params[:description], url: params[:url], screenshot: params[:screenshot])
     end
   end
 
   def destroy
-    
+    student = Student.find_by(id: params[:id])
+    student.delete
+    student.educations.delete
+    student.experiences.delete
+    student.capstones.delete
+    student.skills.delete
   end
 end
