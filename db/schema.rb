b5401c89733b91d88d6ed9038d7d533315b7bd6c
Michael Yatco <mjyatco@gmail.com>
2017-03-13 18:49:20 -0400

    Added API_Key Token Security

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310023008) do
+ActiveRecord::Schema.define(version: 20170313221920) do
 
   create_table "capstones", force: :cascade do |t|
     t.string   "name"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20170310023008) do
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
     t.string   "password_digest"
+    t.string   "api_key"
   end
 
 end
