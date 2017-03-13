124893caa45f49c780451e7bb772d84a4c856d2f
James Chou <jameschou1993@gmail.com>
2017-03-13 19:28:34 -0400

    fixed partials

--- app/controllers/api/v1/resume_controller.rb
+++ app/controllers/api/v1/resume_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::ResumeController < ApplicationController
     @students = Student.all
     render 'index.json.jbuilder'
   end
+
   def show
     @student = Student.find_by(id: params[:id])
   end
@@ -10,24 +11,40 @@ class Api::V1::ResumeController < ApplicationController
   def update
     student =  Student.find_by(id: params[:id])
     student.assign_attributes(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number], short_bio: params[:short_bio], linkedin_url: params[:linkedin_url], twitter_handle: params[:twitter_handle], personal_website_url: params[:personal_website_url], online_resume_url: params[:online_resume_url], github_url: params[:github_url], photo: params[:photo])
-    students.save
+      if student.save
+        redirect_to 'index.json.jbuilder'
+      else 
+        render json: { errors: student.errors.full_messages}
+      end
 
     params[:experiences].each do |experience_hash|
       experience = Experience.find_by(id: experience_hash[:id])
       experience.assign_attributes(start_date: experience_hash[:start_date])
-      experience.save
+      if experience.save
+        redirect_to 'index.json.jbuilder'
+      else 
+        render json: { errors: experience.errors.full_messages}
+      end
     end
 
     params[:skills].each do |skills_hash|
       skill = Skill.find_by(id: skills_hash[:id])
       skill.assign_attributes(skill_name: skills_hash[:skill_name])
-      skill.save
+      if skill.save
+        redirect_to 'index.json.jbuilder'
+      else 
+        render json: { errors: skill.errors.full_messages}
+      end
     end
 
     params[:educations].each do |educations_hash|
       education = Education.find_by(id: educations_hash[:id])
       education.assign_attributes(start_date: params[:start_date], end_date: params[:end_date], degree: params[:degree], university: params[:university], details: params[:details])
-      education.save
+      if education.save
+        redirect_to 'index.json.jbuilder'
+      else 
+        render json: { errors: education.errors.full_messages}
+      end
     end
     params[:capstones].each do |capstones_hash|
       capstone = Capstone.find_by(id: capstones_hash[:id])
@@ -35,7 +52,8 @@ class Api::V1::ResumeController < ApplicationController
       if capstone.save
         redirect_to 'index.json.jbuilder'
       else 
-        
+        render json: { errors: capstone.errors.full_messages}
+      end
     end
   end
 
